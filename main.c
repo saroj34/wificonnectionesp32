6ec0d7ea6251029d2af6eed27a4ce1383599f2ad
saroj kc <kcsaroj034@gmail.com>
2022-06-11 12:03:08 +0545

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,5 @@
-# wificonnectionesp32
-/**
- * @file wifi connection of esp32 as station
- * @author machineer tech
- * @brief 
- * @version 0.1
- * @date 2021-02-27
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
+
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -21,12 +12,9 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-/* The examples use WiFi configuration that you can set via project configuration menu
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_ESP_WIFI_SSID      /*SSID name*/"asus_fbnpa_2.4"
-#define EXAMPLE_ESP_WIFI_PASS      /*SSID password*/"CLFBC6E9D8"
+
+#define EXAMPLE_ESP_WIFI_SSID      /*SSID name*/
+#define EXAMPLE_ESP_WIFI_PASS      /*SSID password*/
 #define EXAMPLE_ESP_MAXIMUM_RETRY  2
 
 /* FreeRTOS event group to signal when we are connected*/
